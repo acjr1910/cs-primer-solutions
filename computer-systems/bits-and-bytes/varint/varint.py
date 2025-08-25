285150509c89e1b645a4eeae165b8f862e780963
acjr1910 <acjr1910@gmail.com>
2025-08-25 17:03:55 +0100

    refactor: use ms black formatter

--- computer-systems/bits-and-bytes/varint/varint.py
+++ computer-systems/bits-and-bytes/varint/varint.py
@@ -1,35 +1,38 @@
 import struct
 
+
 def encode(n):
-  out = []
-  while n > 0:
-    part = n & 0x7f
-    n >>= 7
-    if n > 0:
-      part |= 0x80
-    out.append(part)
-  return bytes(out)
+    out = []
+    while n > 0:
+        part = n & 0x7F
+        n >>= 7
+        if n > 0:
+            part |= 0x80
+        out.append(part)
+    return bytes(out)
+
 
 def decode(varint):
-  n = 0
-  for b in reversed(varint):
-    n <<= 7
-    n |= (b & 0x7f)
-  return n
+    n = 0
+    for b in reversed(varint):
+        n <<= 7
+        n |= b & 0x7F
+    return n
 
-if __name__ == '__main__':
-  cases = (
-    ('1.uint64', b'\x01'),
-    ('150.uint64', b'\x96\x01'),
-    ('maxint.uint64', b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\x01'),
-  )
-  for fname, expectation in cases:
-    with open(fname, 'rb') as file:
-      n = struct.unpack('>Q', file.read())[0]
-      assert encode(n) == expectation
-      assert decode(encode(n)) == n
-  print('ok')
 
-# 0x7F -> 01111111 
+if __name__ == "__main__":
+    cases = (
+        ("1.uint64", b"\x01"),
+        ("150.uint64", b"\x96\x01"),
+        ("maxint.uint64", b"\xff\xff\xff\xff\xff\xff\xff\xff\xff\x01"),
+    )
+    for fname, expectation in cases:
+        with open(fname, "rb") as file:
+            n = struct.unpack(">Q", file.read())[0]
+            assert encode(n) == expectation
+            assert decode(encode(n)) == n
+    print("ok")
+
+# 0x7F -> 01111111
 # 150  -> 10010110
 # 128  -> 10000000
